763a1eab2d418f9e68b4a3978b9cbe3865bb8a7c
roeihaviv <rhaviv@chicagobooth.edu>
2022-04-29 00:06:59 +0000

    Dashboards complete

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -26,11 +26,11 @@ class CurrenciesController < ApplicationController
   end
 
   def currency_converter
-    @raw_data = open("https://api.exchangerate.host/symbols").read
-    @parsed_data = JSON.parse(@raw_data)
-    @symbols_hash = @parsed_data.fetch("symbols")
+    @raw_exchange = open("https://api.exchangerate.host/convert?from=USD&to=EUR").read
+    @parsed_exchange = JSON.parse(@raw_exchange)
+    @exchange_hash = @parsed_exchange.fetch("result")
     
-    @array_of_symbols = @symbols_hash.keys
+    @array_of_exchanges = @exchange_hash
 
     @from_symbol = params.fetch("from_currency")
     @to_symbol = params.fetch("to_currency")
